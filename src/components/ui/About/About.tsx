0662e4ede4d7c9557f78634ad757ab291639f6ed
FarCodeFr <Timo-44@hotmail.com>
2026-03-19 21:54:39 +0100

    Fix gsap + add timeLine for animation

--- src/components/ui/About/About.tsx
+++ src/components/ui/About/About.tsx
@@ -7,27 +7,71 @@ import type { AboutProps } from "../../../types/about.t";
 import Hobby from "./Hobby";
 import Reseau from "./Reseau";
 import { useGSAP } from "@gsap/react";
+import SplitText from "gsap/SplitText";
 
-gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(useGSAP, SplitText);
 
 function About({ open, setOpen }: AboutProps) {
+  // Gestion ouverture fermeture du panel
   const containerRef = useRef<HTMLDivElement>(null);
   const panelRef = useRef<HTMLDivElement>(null);
   const trackRef = useRef<HTMLDivElement>(null);
+  // Gestion d'apparition des éléments du panel
+  const titleHeroAboutRef = useRef<HTMLHeadingElement>(null);
+  const bioHeroAboutRef = useRef<HTMLElement>(null);
 
+  // Container du panel about
   useGSAP(
     () => {
       const overlay = containerRef.current;
       const panel = panelRef.current;
+      const title = titleHeroAboutRef.current;
+      const bio = bioHeroAboutRef.current;
+      const logo = trackRef.current?.querySelectorAll("[data-logo-item]");
+      const track = trackRef.current;
 
-      if (!overlay || !panel) return;
+      if (
+        !overlay ||
+        !panel ||
+        !title ||
+        !bio ||
+        !logo?.length ||
+        !trackRef.current ||
+        !track
+      )
+        return;
 
-      gsap.killTweensOf([overlay, panel]);
+      // Animation Text
+      const splitTitle = new SplitText(title, { type: "chars" });
+      const splitBio = new SplitText(bio, { type: "lines" });
+
+      // Caroussel
+      let carouselTween: gsap.core.Tween | null = null;
+      const handleEnterCaroussel = () => {
+        carouselTween?.timeScale(0.5);
+      };
+      const handleLeaveCaroussel = () => {
+        carouselTween?.timeScale(1);
+      };
+      track.addEventListener("mouseenter", handleEnterCaroussel);
+      track.addEventListener("mouseleave", handleLeaveCaroussel);
+
+      gsap.killTweensOf([
+        overlay,
+        panel,
+        splitTitle.chars,
+        splitBio.lines,
+        bio,
+        logo,
+        trackRef.current,
+      ]);
 
       if (open) {
         overlay.classList.add(styles.visible);
 
-        gsap.fromTo(
+        const tl = gsap.timeline();
+
+        tl.fromTo(
           panel,
           { x: "100%" },
           {
@@ -35,7 +79,48 @@ function About({ open, setOpen }: AboutProps) {
             duration: 1.5,
             ease: "power4.out",
           },
-        );
+        )
+          .from(
+            splitTitle.chars,
+            {
+              y: "random(-50, 50)",
+              opacity: 0,
+              stagger: 0.02,
+              duration: 0.6,
+            },
+            "-=0.35",
+          )
+          .from(
+            splitBio.lines,
+            {
+              y: 30,
+              opacity: 0,
+              stagger: 0.05,
+            },
+            "-=0.45",
+          )
+          .from(
+            logo,
+            {
+              y: 40,
+              opacity: 0,
+              duration: 0.8,
+              stagger: 0.08,
+
+              ease: "elastic.out",
+            },
+            "-=0.2",
+          )
+          .add(() => {
+            if (trackRef.current) {
+              carouselTween = gsap.to(trackRef.current, {
+                xPercent: -30,
+                duration: 10,
+                repeat: -1,
+                ease: "none",
+              });
+            }
+          });
       } else {
         gsap.to(panel, {
           x: "100%",
@@ -46,27 +131,10 @@ function About({ open, setOpen }: AboutProps) {
           },
         });
       }
-    },
-    {
-      scope: containerRef,
-      dependencies: [open],
-    },
-  );
-
-  useGSAP(
-    () => {
-      const track = trackRef.current;
-      if (!track || !open) return;
-
-      const tween = gsap.to(track, {
-        xPercent: -30,
-        duration: 20,
-        repeat: -1,
-        ease: "none",
-      });
-
       return () => {
-        tween.kill();
+        splitTitle.revert();
+        splitBio.revert();
+        carouselTween?.kill();
       };
     },
     {
@@ -82,13 +150,13 @@ function About({ open, setOpen }: AboutProps) {
           Closed
         </button>
         <section className={styles.heros_about}>
-          <h1>
+          <h1 ref={titleHeroAboutRef}>
             Front-end
             <br /> Developer
           </h1>
-          <article>
-            <p>BIO</p>
-            <p>
+          <article ref={bioHeroAboutRef}>
+            <p className={styles.bio_title}>BIO</p>
+            <p className={styles.bio_description}>
               Passionné par l’informatique depuis mon plus jeune âge, j’ai
               récemment suivi une formation intensive de 5 mois (équivalent Bac
               +2) en développement web Full Stack. <br />
@@ -111,6 +179,7 @@ function About({ open, setOpen }: AboutProps) {
         </section>
         <section className={styles.logo_about}>
           <div ref={trackRef} className={styles.logo_track}>
+            <CarouselLogo />
             <CarouselLogo />
           </div>
         </section>
